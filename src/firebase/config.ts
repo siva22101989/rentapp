7684e4801d1c02643f03d6eb6cd90a1a224ff3dd
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-01-27 09:13:54 +0000

    api key pls

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "siva.firebaseapp.com",
   projectId: "siva",
-  storageBucket: "siva.firebasestorage.app",
+  storageBucket: "siva.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID",
   measurementId: "YOUR_MEASUREMENT_ID"

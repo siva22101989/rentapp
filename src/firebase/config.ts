596918d5e181a0780a0e89267ed8cb3b5f086c3d
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-01-27 09:12:57 +0000

    firebase console to connect and data save in siva ok

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyB5-XstMS7j6pTn0kOLlAi_FMZnVIWyazc",
-  authDomain: "vocal-byte-457809-n2.firebaseapp.com",
-  projectId: "vocal-byte-457809-n2",
-  storageBucket: "vocal-byte-457809-n2.firebasestorage.app",
-  messagingSenderId: "555929754101",
-  appId: "1:555929754101:web:a7a9a7343aadd5323f28bc",
-  measurementId: "G-FZJBY78WBF"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "siva.firebaseapp.com",
+  projectId: "siva",
+  storageBucket: "siva.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };

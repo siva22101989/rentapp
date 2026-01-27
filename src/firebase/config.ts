5ec56d8d0c70019db1ee531ba475df05797516fd
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-01-27 09:16:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "siva.firebaseapp.com",
-  projectId: "siva",
-  storageBucket: "siva.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "vocal-byte-457809-n2",
+  "appId": "1:555929754101:web:67b54caec201c44a3f28bc",
+  "apiKey": "AIzaSyB5-XstMS7j6pTn0kOLlAi_FMZnVIWyazc",
+  "authDomain": "vocal-byte-457809-n2.firebaseapp.com",
+  "messagingSenderId": "555929754101"
 };
+

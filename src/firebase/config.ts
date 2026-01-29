babee63aaf59ea1607c82dd3d16314f102cb873e
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-01-29 08:27:46 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,13 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  "projectId": "vocal-byte-457809-n2",
-  "appId": "1:555929754101:web:67b54caec201c44a3f28bc",
-  "apiKey": "AIzaSyB5-XstMS7j6pTn0kOLlAi_FMZnVIWyazc",
-  "authDomain": "vocal-byte-457809-n2.firebaseapp.com",
-  "messagingSenderId": "555929754101"
+  apiKey: "AIzaSyB5-XstMS7j6pTn0kOLlAi_FMZnVIWyazc",
+  authDomain: "vocal-byte-457809-n2.firebaseapp.com",
+  projectId: "vocal-byte-457809-n2",
+  storageBucket: "vocal-byte-457809-n2.firebasestorage.app",
+  messagingSenderId: "555929754101",
+  appId: "1:555929754101:web:a7a9a7343aadd5323f28bc",
+  measurementId: "G-FZJBY78WBF"
 };
+
 

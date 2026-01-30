0d43c922100e208eb60384907d0cc89462e40331
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-01-30 03:35:28 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/components/lots/lots-client.tsx
+++ src/components/lots/lots-client.tsx
@@ -1,7 +1,6 @@
-
 'use client';
 
-import { useTransition, useState } from 'react';
+import { useTransition, useState, useMemo } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';

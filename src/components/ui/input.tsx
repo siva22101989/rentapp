89d9885f0490a4dc331ba13f562352531a17c82f
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-02-18 09:59:53 +0000

    Try fixing this error: `Console Error: A component is changing an uncont

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, value, ...props }, ref) => {
     return (
       <input
         type={type}
@@ -13,6 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
+        value={value ?? ''}
         {...props}
       />
     )

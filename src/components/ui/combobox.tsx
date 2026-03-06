0201c2774cb85cf45a3cfd55b8649bf28e95a1ec
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-03-06 03:20:26 +0000

    when customer selection that diloag box is disappearing y

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -45,7 +45,7 @@ export function Combobox({
   const [open, setOpen] = React.useState(false)
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal={true}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"

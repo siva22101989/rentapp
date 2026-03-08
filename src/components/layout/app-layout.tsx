762c92343e7a681b7d1c7d3ea521e93c1b24cdb3
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-03-08 07:30:42 +0000

    here delete pick a date range

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -56,12 +56,9 @@ function LiveClock() {
 
 function DateFilters() {
     const { 
-        dateRange, 
-        setDateRange,
         financialYear,
         handleFinancialYearChange,
         financialYears,
-        resetFilters
     } = useDateFilter();
 
     return (
@@ -79,40 +76,6 @@ function DateFilters() {
                     ))}
                 </SelectContent>
             </Select>
-            <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                id="date"
-                variant={"outline"}
-                className="w-[240px] h-8 justify-start text-left font-normal text-xs"
-                >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {dateRange?.from ? (
-                    dateRange.to ? (
-                    <>
-                        {format(dateRange.from, "LLL dd, y")} -{" "}
-                        {format(dateRange.to, "LLL dd, y")}
-                    </>
-                    ) : (
-                    format(dateRange.from, "LLL dd, y")
-                    )
-                ) : (
-                    <span>Pick a date range</span>
-                )}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="end">
-                <Calendar
-                initialFocus
-                mode="range"
-                defaultMonth={dateRange?.from}
-                selected={dateRange}
-                onSelect={setDateRange}
-                numberOfMonths={2}
-                />
-            </PopoverContent>
-            </Popover>
-            {dateRange && <Button variant="ghost" size="icon" className="h-8 w-8" onClick={resetFilters}><X className="h-4 w-4" /></Button>}
         </div>
     );
 }

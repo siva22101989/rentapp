ecc82e03ca59da8be567fed83ce447a135f2a44c
Siva Sandeep Reddy <sivasandeepreddy01@gmail.com>
2026-04-07 02:04:24 +0000

    only this colour is same and full app colour is change to blue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,10 @@ export default {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
+        logo: {
+          DEFAULT: 'hsl(var(--logo-color))',
+          foreground: 'hsl(var(--logo-color-foreground))',
+        },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
